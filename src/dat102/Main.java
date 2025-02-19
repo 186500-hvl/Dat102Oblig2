2e12152ca360d669715e8c7dc924060a3af0bc01
186500-hvl <eirikinnselset@hotmail.com>
2025-02-19 14:27:53 +0100

    hhhh

--- src/dat102/Main.java
+++ src/dat102/Main.java
@@ -1,5 +1,10 @@
 package dat102;
-
 public class Main {
-
+   public static void main(String[] args) {
+       String test1 = "{ [ ( ) ] }"; 
+       String test2 = "{ [ ( ) }";   
+       System.out.println("Test 1: " + ParentesSjekker.sjekkParenteser(test1));
+       System.out.println("Test 2: " + ParentesSjekker.sjekkParenteser(test2));
+   }
 }
+
--- src/dat102/ParentesSjekkerTest.java
+++ src/dat102/ParentesSjekkerTest.java
@@ -1,14 +1,38 @@
 package dat102;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class ParentesSjekkerTest {
+public class ParentesSjekkerTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testKorrektParenteser() {
+        assertTrue(ParentesSjekker.sjekkParenteser("{ [ ( ) ] }"));
+    }
 
+    @Test
+    public void testManglerSluttParentes() {
+        assertFalse(ParentesSjekker.sjekkParenteser("{ [ ( ) }"));
+    }
+
+    @Test
+    public void testManglerStartParentes() {
+        assertFalse(ParentesSjekker.sjekkParenteser("[ ( ) ] }"));
+    }
+
+    @Test
+    public void testFeilRekkefolge() {
+        assertFalse(ParentesSjekker.sjekkParenteser("{ [ ( ] ) }"));
+    }
+
+    @Test
+    public void testKodeMedKorrektParenteser() {
+        String javaprogram = """
+        class HelloWorld {
+            public static void main(String[] args) {
+                System.out.println("Hello World!");
+            }
+        }
+        """;
+        assertTrue(ParentesSjekker.sjekkParenteser(javaprogram));
+    }
 }
