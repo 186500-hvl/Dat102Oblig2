bac8c385fcfb863ce068d522942e552d5c949f3a
186500-hvl <eirikinnselset@hotmail.com>
2025-02-19 14:05:09 +0100

    hm

---
+++ src/dat102/ParentesSjekker.java
@@ -0,0 +1,29 @@
+package dat102;
+import java.util.Stack;
+public class ParentesSjekker {
+   public static boolean sjekkParenteser(String s) {
+       Stack<Character> stabel = new Stack<>();
+      
+       for (char c : s.toCharArray()) {
+           if (erStartParentes(c)) {
+               stabel.push(c);
+           } else if (erSluttParentes(c)) {
+               if (stabel.isEmpty() || !erParentesPar(stabel.pop(), c)) {
+                   return false;
+               }
+           }
+       }
+       return stabel.isEmpty();
+   }
+   private static boolean erStartParentes(char c) {
+       return c == '(' || c == '[' || c == '{';
+   }
+   private static boolean erSluttParentes(char c) {
+       return c == ')' || c == ']' || c == '}';
+   }
+   private static boolean erParentesPar(char start, char slutt) {
+       return (start == '(' && slutt == ')') ||
+              (start == '[' && slutt == ']') ||
+              (start == '{' && slutt == '}');
+   }
+}
---
+++ src/dat102/ParentesSjekkerTest.java
@@ -0,0 +1,14 @@
+package dat102;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParentesSjekkerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
